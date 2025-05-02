252eded3b611463e4763fa195828321346aa3df9
Gautam696969 <palgautam69@gmail.com>
2025-05-02 13:35:42 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default({
   plugins: [react()],
   base: '/Education_AI/',
   resolve: {
